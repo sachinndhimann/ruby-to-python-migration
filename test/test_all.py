d81eede9534d2ed2d80c323775af1f8263390257
sachin.dhiman <40537108+sachinndhimann@users.noreply.github.com>
2024-05-30 17:36:39 +0530

    Update test_all.py
    uncommented the code

--- test/test_all.py
+++ test/test_all.py
@@ -7,78 +7,78 @@ from src import autofix_json as af
 from src import rpsec_to_python as rp
 
 
-# class TestJsonAutofix(unittest.TestCase):
+class TestJsonAutofix(unittest.TestCase):
 
-#     def test_correct_json(self):
-#         json_str = '{ "item": { "name": "item3" }}'
-#         self.assertEqual(json.loads(json_str), json.loads(af.autofix_json_string(json_str)))
+    def test_correct_json(self):
+        json_str = '{ "item": { "name": "item3" }}'
+        self.assertEqual(json.loads(json_str), json.loads(af.autofix_json_string(json_str)))
     
-#     def test_missing_closing_brace(self):
-#         json_str = '{ "item": { "name": "item3" }'
-#         fixed_str = af.autofix_json_string(json_str)
-#         self.assertEqual(json.loads(fixed_str), {"item": {"name": "item3"}})
+    def test_missing_closing_brace(self):
+        json_str = '{ "item": { "name": "item3" }'
+        fixed_str = af.autofix_json_string(json_str)
+        self.assertEqual(json.loads(fixed_str), {"item": {"name": "item3"}})
     
-#     def test_missing_closing_bracket(self):
-#         json_str = '[1, 2, 3'
-#         fixed_str = af.autofix_json_string(json_str)
-#         self.assertEqual(json.loads(fixed_str), [1, 2, 3])
+    def test_missing_closing_bracket(self):
+        json_str = '[1, 2, 3'
+        fixed_str = af.autofix_json_string(json_str)
+        self.assertEqual(json.loads(fixed_str), [1, 2, 3])
 
-# class TestRubyToJsonConversion(unittest.TestCase):
+class TestRubyToJsonConversion(unittest.TestCase):
 
-#     def test_simple_ruby_hash(self):
-#         ruby_hash = "{ item: 'item3' }"
-#         expected = {"item": "item3"}
-#         self.assertEqual(rp.ruby_to_json(ruby_hash), expected)
+    def test_simple_ruby_hash(self):
+        ruby_hash = "{ item: 'item3' }"
+        expected = {"item": "item3"}
+        self.assertEqual(rp.ruby_to_json(ruby_hash), expected)
     
-#     def test_nested_ruby_hash(self):
-#         ruby_hash = "{ item: { name: 'item3' } }"
-#         expected = {"item": {"name": "item3"}}
-#         self.assertEqual(rp.ruby_to_json(ruby_hash), expected)
+    def test_nested_ruby_hash(self):
+        ruby_hash = "{ item: { name: 'item3' } }"
+        expected = {"item": {"name": "item3"}}
+        self.assertEqual(rp.ruby_to_json(ruby_hash), expected)
     
-#     def test_ruby_hash_with_numbers(self):
-#         ruby_hash = "{ count: 5, items: [1, 2, 3] }"
-#         expected = {"count": 5, "items": [1, 2, 3]}
-#         self.assertEqual(rp.ruby_to_json(ruby_hash), expected)
+    def test_ruby_hash_with_numbers(self):
+        ruby_hash = "{ count: 5, items: [1, 2, 3] }"
+        expected = {"count": 5, "items": [1, 2, 3]}
+        self.assertEqual(rp.ruby_to_json(ruby_hash), expected)
 
-# class TestRSpecParsing(unittest.TestCase):
+class TestRSpecParsing(unittest.TestCase):
 
-#     def setUp(self):
-#         self.rspec_content = """
-#         require 'rails_helper'
+    def setUp(self):
+        self.rspec_content = """
+        require 'rails_helper'
 
-#         RSpec.describe 'API', type: :request do
-#           it 'returns a list of items' do
-#             get '/api/items'
-#             expect(response).to have_http_status(200)
-#             expect(response.body).to include('item1', 'item2')
-#           end
+        RSpec.describe 'API', type: :request do
+          it 'returns a list of items' do
+            get '/api/items'
+            expect(response).to have_http_status(200)
+            expect(response.body).to include('item1', 'item2')
+          end
 
-#           it 'creates an item' do
-#             post '/api/items', params: { item: { name: 'item3' } }
-#             expect(response).to have_http_status(201)
-#             expect(response.body).to include('item3')
-#           end
-#         end
-#         """
+          it 'creates an item' do
+            post '/api/items', params: { item: { name: 'item3' } }
+            expect(response).to have_http_status(201)
+            expect(response.body).to include('item3')
+          end
+        end
+        """
 
-#     def test_parse_rspec(self):
-#         expected_tests = [
-#             {
-#                 'method': 'get',
-#                 'url': '/api/items',
-#                 'params': None,
-#                 'status': '200',
-#                 'body': ['item1', 'item2']
-#             },
-#             {
-#                 'method': 'post',
-#                 'url': '/api/items',
-#                 'params': {"item": {"name": "item3"}},
-#                 'status': '201',
-#                 'body': ['item3']
-#             }
-#         ]
-#         self.assertEqual(rp.parse_rspec(self.rspec_content), expected_tests)
+    def test_parse_rspec(self):
+        expected_tests = [
+            {
+                'method': 'get',
+                'url': '/api/items',
+                'params': None,
+                'status': '200',
+                'body': ['item1', 'item2']
+            },
+            {
+                'method': 'post',
+                'url': '/api/items',
+                'params': {"item": {"name": "item3"}},
+                'status': '201',
+                'body': ['item3']
+            }
+        ]
+        self.assertEqual(rp.parse_rspec(self.rspec_content), expected_tests)
 
 class TestPythonTestGeneration(unittest.TestCase):
 
